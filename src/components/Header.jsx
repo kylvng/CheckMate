52509e56e5ccd1820445a797f37997fb38b7d7ba
Andriy-Luchko <andriy.luchko@gmail.com>
2024-04-20 21:42:41 -0400

    header creation

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h2 className="headerText" >CheckMate</h2>
+      <img className="foodIcon" src="./foodicon.png" alt="Food Icon" />
+    </div>
+  );
+}
+
+export default Header;
